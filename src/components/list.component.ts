280e9edcedb4d51a23868c1ce57b94eb78b19bcd
zoltan <zkubanyi@gmail.com>
2017-07-24 15:48:51 -0700

    This is an Angular 2 sample we are using as the start template. It is a simple crud app that we will modify.

---
+++ src/components/list.component.ts
@@ -0,0 +1,29 @@
+///<reference path="../../node_modules/angular2/typings/browser.d.ts"/>
+import { Component, OnInit } from 'angular2/core';
+import { ROUTER_DIRECTIVES } from 'angular2/router';
+
+import { Employee } from '../models/employee';
+import { EmployeeListServiceComponent } from '../services/employee-list-service.component';
+
+@Component({
+  selector: 'employee-list',
+  templateUrl: 'src/pages/employee-list.component.html',
+  directives: [ROUTER_DIRECTIVES],
+  providers: [EmployeeListServiceComponent]
+})
+
+export class EmployeeListComponent implements OnInit {
+  public employees: Employee[];
+  public errorMessage: string;
+
+  constructor(
+    private _listingService: EmployeeListServiceComponent
+  ){}
+
+  ngOnInit() {
+    this._listingService.getEmployees().subscribe(
+                     employees => this.employees = employees,
+                     error =>  this.errorMessage = <any>error
+                   );
+  }
+}
